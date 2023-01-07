462f7a16119460ea0c30d9470189c2ef7a097034
Miguel Gargallo <mgargallo16@gmail.com>
2023-01-07 12:17:54 +0100

    1.0.1 Post

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Miguel Gargallo | Tech Review.';
+export const SITE_DESCRIPTION = 'Welcome to Miguel Gargallo Tech Review!';
+export const SITE_URL = 'https://miguelgargallo.com';
+export const SITE_IMAGE = 'https://miguelgargallo.com/images/og-image.png';
+export const TWITTER_HANDLE = '@miguelgargallo';
+export const TWITTER_SITE = '@miguelgargallo';
+export const TWITTER_CARD_TYPE = 'summary_large_image';
+export const GA_TRACKING_ID = 'UA-XXXXX-X';
+export const DATE_FORMAT = 'MMMM Do, YYYY';
+export const AUTHOR = 'Miguel Gargallo';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,11 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { SITE_TITLE, SITE_DESCRIPTION, AUTHOR } from '../config';
 
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
+		author: AUTHOR,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items: import.meta.glob('./blog/**/*.{md,mdx,pen}'),
 	});
