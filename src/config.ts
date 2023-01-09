3b15f3df3e7f227dd87b26f41b8e968888e1e594
Miguel Gargallo <mgargallo16@gmail.com>
2023-01-09 02:39:06 +0100

    1.0.32 Marketing

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tech Review.';
+export const SITE_TITLE = 'About AI and Tech Review.';
 export const SITE_SEPARATOR = ' | ';
 export const SITE_DESCRIPTION = 'Welcome to Miguel Gargallo Tech Review!';
 export const SITE_URL = 'https://miguelgargallo.com';
