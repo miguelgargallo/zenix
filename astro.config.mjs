0a4bbee9b01fb6c45912a2849a821c9fb3fe6092
Miguel Gargallo <mgargallo16@gmail.com>
2023-01-09 17:07:58 +0100

    1.0.37 React not inside

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://miguelgargallo.com',
-  integrations: [mdx(), sitemap(), react()]
+	site: 'https://miguelgargallo.com',
+	integrations: [mdx(), sitemap()],
 });
