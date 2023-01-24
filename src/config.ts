ed7ad5a398cc107f2e3555589697951c3a892435
miguelgargallo <andorra.mg@gmail.com>
2023-01-24 21:56:12 +0100

    github 1.0.108 | npm 0.0.44 | Languages

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'About AI and Tech Review.';
+export const SITE_TITLE = 'Invest less than 2 minutes a day to become a better developer';
 export const SITE_SEPARATOR = ' | ';
 export const SITE_DESCRIPTION = 'Welcome to Miguel Gargallo Tech Review!';
 export const SITE_URL = 'https://miguelgargallo.com';
@@ -11,3 +11,4 @@ export const TWITTER_SITE = '@miguelgargallo';
 export const TWITTER_CARD_TYPE = 'summary_large_image';
 export const DATE_FORMAT = 'MMMM Do, YYYY';
 export const AUTHOR = 'Miguel Gargallo';
+export const FOOTER_TEXT = 'Invest, Read, Code, now you can Deploy.';
